45b786585378fa7a766e18f260ebd525483503f2
pc0523 <molavef@ing.ucsc.cl>
2026-06-08 09:48:27 -0400

    colores reasignados

--- app/src/main/java/com/camposocampoolavevargas/proyecto/ui/theme/Color.kt
+++ app/src/main/java/com/camposocampoolavevargas/proyecto/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.camposocampoolavevargas.proyecto.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Fondo principal
+val DarkBackground = Color(0xFF1E293B)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Tarjetas
+val CardBackground = Color(0xFF243447)
+
+// Primario (naranja)
+val PrimaryOrange = Color(0xFFFFA726)
+
+// Secundario
+val SecondaryOrange = Color(0xFFFFB74D)
+
+// Texto
+val TextWhite = Color(0xFFFFFFFF)
+
+// Estados
+val SuccessGreen = Color(0xFF66BB6A)
+val WarningYellow = Color(0xFFFFCA28)
+val ErrorRed = Color(0xFFEF5350)
