4e765d5444be30740bd66b1d4c454759fb15cc5d
pc0523 <molavef@ing.ucsc.cl>
2026-06-08 09:10:14 -0400

    Clase Usuario

--- app/src/main/java/com/camposocampoolavevargas/proyecto/model/Usuario.kt
+++ app/src/main/java/com/camposocampoolavevargas/proyecto/model/Usuario.kt
@@ -1,2 +1,13 @@
 package com.camposocampoolavevargas.proyecto.model
 
+
+data class Usuario(
+    val nombre: String,
+    val fechaNacimiento: String,
+    val region: String,
+    val comuna: String,
+    val universidad: String,
+    val carrera: String,
+    val correo: String,
+    val password: String
+)
