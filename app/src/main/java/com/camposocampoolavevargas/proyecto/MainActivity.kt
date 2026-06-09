dbbdb4d6baf3bce457d05b5a30b1c0f2defc566b
Matito <86012198+MatiasOlave1@users.noreply.github.com>
2026-06-09 18:07:51 -0400

    Prueba Registro

--- app/src/main/java/com/camposocampoolavevargas/proyecto/MainActivity.kt
+++ app/src/main/java/com/camposocampoolavevargas/proyecto/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.*
-import com.camposocampoolavevargas.proyecto.screens.LoginScreen
-import com.camposocampoolavevargas.proyecto.screens.UserRegisterScreen
+import com.camposocampoolavevargas.proyecto.screens.SleepRegisterScreen
 import com.camposocampoolavevargas.proyecto.ui.theme.ProyectoTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +15,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             ProyectoTheme {
-
-                var mostrarRegistro by remember {
-                    mutableStateOf(false)
-                }
-
-                if (mostrarRegistro) {
-
-                    UserRegisterScreen(
-                        onBackToLogin = {
-                            mostrarRegistro = false
-                        }
-                    )
-
-                } else {
-
-                    LoginScreen(
-                        onRegisterClick = {
-                            mostrarRegistro = true
-                        }
-                    )
-                }
+                SleepRegisterScreen()
             }
         }
     }
